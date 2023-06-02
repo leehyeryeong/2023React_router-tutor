e527817ecacb0615b5e845cffb36132a52a8082a
leehyeryeong <w2108@e-mirim.hs.kr>
2023-06-02 12:13:54 +0900

    게시글 목록을 보여주는 페이지, 게시글 읽는 페이지

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import About from "./pages/about";
-import Home from "./pages/home";
-import Profile from "./pages/profiles";
+import About from "./pages/About";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/articles" element={<Articles />} />
+      <Route path="/articles/:id" element={<Article />} />
     </Routes>
   );
 };
